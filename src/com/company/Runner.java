599b857939310d0d6e6b195304ca1bc9c35c95fb
Bekulan <totonya1509@bk.ru>
2019-11-14 20:20:40 +0600

    reformat code

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Runner extends Thread {
 
-   private int number = 1;
+    private int number = 1;
 
     public int getNumber() {
         return number;
@@ -14,15 +14,15 @@ public class Runner extends Thread {
 
     @Override
     public void run() {
-        System.out.println(getName() +" took the stick");
+        System.out.println(getName() + " took the stick");
         if (getNumber() - 1 == 5) {
             System.out.println("Runner 5 is finished");
-            System.out.println(getName()+" run to Runner 4");
-        }else if (getNumber()==1){
-            System.out.println(getName()+" ran to Runner "+getNumber());
-            System.out.println("Runner "+getNumber()+" took the stick");
-        }else {
-            System.out.println(getName()+" ran to Runner "+getNumber());
+            System.out.println(getName() + " run to Runner 4");
+        } else if (getNumber() == 1) {
+            System.out.println(getName() + " ran to Runner " + getNumber());
+            System.out.println("Runner " + getNumber() + " took the stick");
+        } else {
+            System.out.println(getName() + " ran to Runner " + getNumber());
         }
         try {
             sleep(5000);
@@ -31,11 +31,10 @@ public class Runner extends Thread {
         }
 
 
-
     }
 
 
-    }
+}
 
 
 
